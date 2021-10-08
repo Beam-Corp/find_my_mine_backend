c62d1b33074fbee71b047fa30ba843740e07592b
siradheek <siradhee_k@hotmail.com>
2021-10-08 14:59:23 +0700

    add mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://beam-corp:${process.env.db_key}@cluster0.x6626.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
