bacd5775d566b1041389605b0c045bd2a6c27562
siradheek <siradhee_k@hotmail.com>
2021-10-28 15:15:18 +0700

    fix logic & set cors credentials

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: process.env.ORIGIN || "*",
+    origin: process.env.ORIGIN || "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
+    credentials:true
   })
   app.use(cookieParser());
   await app.listen(process.env.PORT || 8000)
