b78fac5c5f2fe956240f91bc48d68c4b798090dd
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-28 11:30:38 +0700

    style: prettier autoformat

--- src/room/room.events.ts
+++ src/room/room.events.ts
@@ -1,15 +1,15 @@
 export enum RoomEvents {
-  CREATE = "create",
-  ON_CREATE = "on-create",
-  LEAVE = "leave",
-  ON_LEAVE = "on-leave",
-  JOIN = "join",
+  CREATE = 'create',
+  ON_CREATE = 'on-create',
+  LEAVE = 'leave',
+  ON_LEAVE = 'on-leave',
+  JOIN = 'join',
   ON_JOIN = 'on-join',
-  INTRODUCE = "introduce",
-  GET_PLAYERS = "get-players",
-  ON_GET_PLAYERS="on-get-players"
+  INTRODUCE = 'introduce',
+  GET_PLAYERS = 'get-players',
+  ON_GET_PLAYERS = 'on-get-players',
 }
 export interface Introduction {
-  username: string,
+  username: string
   roomId: string
 }
