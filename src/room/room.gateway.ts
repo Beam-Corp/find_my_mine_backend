73115bb995ae4ef9cbf047dec0b1ca66c2a9e37b
siradheek <siradhee_k@hotmail.com>
2021-11-03 15:33:20 +0700

    store clientState in socket

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -4,7 +4,7 @@ import { WebSocketGateway, WebSocketServer, SubscribeMessage, WsException } from
 import { Socket, Server } from 'socket.io'
 import { v4 as uuidv4 } from 'uuid'
 
-import { Introduction, RoomEvents } from './room.events'
+import { ClientState, Introduction, RoomEvents } from './room.events'
 
 @WebSocketGateway({ cors: true })
 export class RoomGateway {
@@ -12,10 +12,15 @@ export class RoomGateway {
   private logger: Logger = new Logger('RoomGateway')
 
   handleDisconnect(client: Socket) {
+    if (!!client.data.roomId) {
+      this.server.to(client.data.roomId).emit(RoomEvents.LEAVE,client.data.username);
+      client.data.roomId = ""
+    }
     this.logger.log(`Client disconnected: ${client.id}`)
   }
 
   handleConnection(client: Socket) {
+    client.data = {} as ClientState
     this.logger.log(`Client connected: ${client.id}`)
   }
   @SubscribeMessage(RoomEvents.ON_CREATE)
@@ -23,6 +28,7 @@ export class RoomGateway {
     const roomId = uuidv4().substring(0, 4)
     if (client.rooms.size !== 0) client.rooms.clear()
     await client.join(roomId)
+    client.data.roomId = roomId;
     this.server.to(roomId).emit(RoomEvents.CREATE, roomId)
   }
 
@@ -32,6 +38,7 @@ export class RoomGateway {
     if (client.rooms.size !== 0) client.rooms.clear()
     if (currentRooms.has(roomId) && currentRooms.get(roomId).size < 2) {
       await client.join(roomId)
+      client.data.roomId = roomId;
       this.logger.log(`${client.id} joined room ${roomId}`)
       this.server.to(roomId).emit(RoomEvents.JOIN, roomId)
     }
@@ -41,6 +48,7 @@ export class RoomGateway {
   @SubscribeMessage(RoomEvents.INTRODUCE)
   async handleIntroduction(client: Socket, { roomId, username }: Introduction) {
     if (!client.rooms.has(roomId) || !this.server.sockets.adapter.rooms.has(roomId)) throw new WsException(`wrong room`)
+    client.data.username = username;
     await client.to(roomId).emit(RoomEvents.INTRODUCE, username)
   }
 
